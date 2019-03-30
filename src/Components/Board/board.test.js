8ab9937edfd98d03219849b4837bb2d6b3874ce2
sivarasan1992 <sivarasan@technohive.tech>
2019-03-30 20:27:49 +0530

    added mount test cases

--- src/Components/Board/board.test.js
+++ src/Components/Board/board.test.js
@@ -1,8 +1,19 @@
 import React from "react";
 import Board from "./board";
-import { shallow } from "enzyme";
+import { shallow, mount } from "enzyme";
 
 it("render without issues", () => {
   const squares = Array(9).fill(null);
   shallow(<Board squares={squares} />);
+});
+it("calls click event on click of a board square", () => {
+  const squares = Array(9).fill(null);
+  const onClick = jest.fn();
+  const wrapper = mount(<Board squares={squares} onClick={onClick} />);
+  wrapper
+    .find("button.square")
+    .first()
+    .simulate("click");
+  console.log(expect(onClick).toBeCalledWith, "expect(onClick)");
+  expect(onClick).toBeCalledWith(0);
 });
